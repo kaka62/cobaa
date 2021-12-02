61be568534934dc681e2b416c7127869b686f6ff
unknown <ridwanfauzi88@gmail.com>
2021-12-02 10:43:57 +0700

    upload projek

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,18 @@
+<h1>Halaman Profile</h1>
+<table>
+    <tr>
+        <td> Nama </td><td><input type="text" name="nama"></td>
+    </tr>
+    <tr>
+        <td> Alamat </td><td><textarea name="nama"></textarea></td>
+    </tr>
+    <tr>
+        <td> Nomor Induk Kependudukan </td><td><input type="text" name="nik"></td>
+    <tr>
+        <td> Nomor Ujian Peserta </td><td><input type="text" name="nomorujian"></td>
+    </tr>
+    <tr>
+        <td> Akun Media Sosial </td><td><input type="text" name="akunmedia"></td>
+    </tr>
+
+</table>
